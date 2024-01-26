da6daa31692eb32fb2c00719d4f5e54ab876ddea
Marcospantunes <marcospantunes203@gmail.com>
2024-01-26 16:39:03 -0300

    Feature
    Implementada lógica de adição e remoção de item na lista utilizando SessionStorage para armazenamento de dados.

--- script.js
+++ script.js
@@ -3,22 +3,55 @@ const btnAddItem = document.querySelector("#addItem");
 const listContainer = document.querySelector("#listContainer");
 const input = document.querySelector("#createTodo");
 const listh2 = document.querySelector("#listh2");
+const filterAll = document.querySelector(".all");
+const filterActive = document.querySelector(".active");
+const filterCompleted = document.querySelector(".completed");
+let tasks = [];
+let all = [];
+
+if(sessionStorage.hasOwnProperty("item")) {
+    all = JSON.parse(sessionStorage.getItem("item"));
+}
+
+function printTasks(list = all) {
+    if(sessionStorage.hasOwnProperty("item") && list.length > 0) {
+        listh2.style.display = "none";
+        listContainer.innerHTML = ""
+        list = JSON.parse(sessionStorage.getItem("item"));
+        list.map((item) => {
+            listContainer.innerHTML += `
+                <li id=${String(item.id)} class="itemContainer ${item.noFilter} ${item.status}">
+                    <div>
+                        <button class="checkButton"></button>
+                        <p>${item.taskName}</p>
+                    </div>
+                    <button class="deleteButton"><img src="images/icon-cross.svg" alt="delete" class="deleteButton"></button>
+                </li>
+            `
+        })
+    } else {
+        listh2.style.display = "block";
+        listContainer.appendChild(listh2)
+    }
+}
+
+printTasks()
 
 btnAddItem.addEventListener("click", () => {
     if(input.value != "") {
-        listh2.style.display = "none";
-        listContainer.innerHTML += `
-            <li class="itemContainer">
-                <div>
-                    <button class="checkButton""></button>
-                    <p>${input.value}</p>
-                </div>
-                <button class="deleteButton"><img src="images/icon-cross.svg" alt="delete" class="deleteButton"></button>
-            </li>
-        `;
+        all.push({
+            id: Math.floor(Math.random() * (1000 - 1 + 1)),
+            noFilter: "all",
+            status: "active",
+            taskName: input.value
+        })
+        sessionStorage.setItem("item", JSON.stringify(all));
+
         input.value = "";
+        
+        printTasks(all)
     } else {
-        console.log("Digite o nome da tarefa!");
+        input.value = "Digite o nome da tarefa!";
     }
     
 })
@@ -28,17 +61,27 @@ document.addEventListener("click", (e) => {
     const itemContainer = target.closest("div");
     const itemList = target.closest("li");
 
-    if (!parent) {
-        return;
-    }
+    if (!parent) return;
 
     if (target.classList.contains("checkButton")) {
         target.classList.toggle("checked");
+        itemList.classList.toggle("active");
+        itemList.classList.toggle("completed");
         itemContainer.children.item(1).classList.toggle("checked");
     }
 
     if (target.classList.contains("deleteButton")) {
         itemList.remove();
-
+        sessionStorage.setItem("item", JSON.stringify(all.filter((item) => item.id != itemList.id)))
+        all = JSON.parse(sessionStorage.getItem("item"));
+        printTasks(all)
     }
 })
+
+filterAll.addEventListener(("click"), (e) => {
+    filter(e.target.classList[1])
+})
+
+function filter(target) {
+   
+}
