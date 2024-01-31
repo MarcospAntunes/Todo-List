5871511f9733cc3a4a411865fde18f824e3c6971
Marcospantunes <marcospantunes203@gmail.com>
2024-01-31 18:48:54 -0300

    Removido código desnecessário

--- script/clearAll.js
+++ script/clearAll.js
@@ -2,8 +2,6 @@ import printTasks from "./printTasks.js";
 
 function clearAll(listh2, listContainer) {
     sessionStorage.removeItem("item");
-    sessionStorage.removeItem("active");
-    sessionStorage.removeItem("completed");
 
     printTasks([], listh2, listContainer);
 }
